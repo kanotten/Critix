d4f0fdc2c31f4cdfd30d132102af3bee43a1f74b
PatrickRoethe <146090971+PatrickRoethe@users.noreply.github.com>
2025-03-21 10:24:10 +0100

    Add footer to mainlayout
    - simple styling for footer (baseline)
    - add footer to mainlayout
    - tested on app.jsx since i have some routing issues. (dont have layout.jsx)
    - had issues with critixlogo in navbar so commented out.

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,3 +1,4 @@
+import Footer from "..//components/Footer";
 import Navbar from "../components/Navbar";
 
 const MainLayout = ({ children }) => {
@@ -5,6 +6,7 @@ const MainLayout = ({ children }) => {
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <main className="flex-1">{children}</main>
+      <Footer />
     </div>
   );
 };
