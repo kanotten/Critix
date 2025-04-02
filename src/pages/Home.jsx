240071800c0926ac9b751fda2656127476ec5d49
PatrickRoethe <146090971+PatrickRoethe@users.noreply.github.com>
2025-04-02 13:59:30 +0200

    Update Home.jsx
    changed emoji

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -180,7 +180,7 @@ const Home = () => {
       </h1>
       {filteredMovies.length === 0 ? (
         <div className="text-center mt-10 text-gray-600 text-lg">
-          😅 Oops! No movies found. Try adjusting your search or filters.
+          😞 Oops! No movies found. Try adjusting your search or filters.
         </div>
       ) : (
         <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
